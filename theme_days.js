dc1132ad30b48ed512ebf9bd91a7949645e131e7
andreas <andreas.a.hoglund@afry.com>
2023-11-07 16:44:51 +0100

    added theme day info

--- theme_days.js
+++ theme_days.js
@@ -322,8 +322,11 @@ const dateTextMap = new Map([
 
 function getThemeDay() {
     const now = new Date();
-    const themeDayText = document.querySelector("#theme-day-div h1");
-    themeDayText.textContent = "Todays theme: " + dateTextMap.get(now.getDate() + ", " + (Number)(now.getMonth()+1))
+    const todaysThemeText = document.querySelector("#theme-day-div h1");
+    const themeDayText = document.querySelector("#theme-day-div h2");
+    todaysThemeText.textContent = "Todays theme:";
+    themeDayText.textContent = dateTextMap.get(now.getDate() + ", " + (Number)(now.getMonth()+1))
+
 }
 
 getThemeDay()
