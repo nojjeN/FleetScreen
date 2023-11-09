de90b4eb58a387f206994ea3cab6f2db6d8dd4ac
andreas <andreas.a.hoglund@afry.com>
2023-11-09 14:16:06 +0100

    added name days

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ function updateProgressBar() {
     "Äntligen är det snart fredag!",
     "Asking Lassing about the hoodies...",
     "Installing clothing pins in the celing…",
-    "Praying for Wolgfang…",
+    "Praying for Wolfgang…",
     "Smasking in each others ears…",
     "Swimming in padel balls…",
     "Admiring Adam's alternating accordion ambition…",
@@ -118,7 +118,15 @@ function updateProgressBar() {
     "Welcome to gatubråkstakens paradis...",
     "Accepting threatening fika invitations...",
     "Link launching our lunch boxes...",
-    "Microdosing Andreas homebrewn coffee in secret..."
+    "Microdosing Andreas homebrewn coffee in secret...",
+    "Adjusting the 100 meter lightsling...",
+    "Giving back Rubiks his cubes...",
+    "Dodging all possible Rubiks cube solutions...",
+    "Applying cube lube...",
+    "Forcing Julmust upon Adam's existence...",
+    "Applying aspartame...",
+    "Acknowledging Andreas absolutely abnormal aspartame aspirations...",
+    "Phosphorylating Philip's phishing photos"
 ]
 
 var shuffledRoomStatuses = [...roomStatuses];
@@ -133,8 +141,8 @@ function shuffleArray(array) {
 }
 
 function selectAndDisplayRandomRoomStatus() {
-    const themeDayText = document.querySelector("#room-status-div h1");
-    themeDayText.textContent = getNextSentence();
+    const roomStatusText = document.querySelector("#room-status-div h1");
+    roomStatusText.textContent = getNextSentence();
 }
 
 function getNextSentence() {
