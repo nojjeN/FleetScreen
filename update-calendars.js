46e5dab1aad66f5e8443dec7a642d7cad49a216d
Adam Andersson <adam.andersson@afry.com>
2023-11-29 16:40:32 +0100

    Update Calendars

--- update-calendars.js
+++ update-calendars.js
@@ -2,204 +2,116 @@ function updateCalendars() { jsonString = [
   [
     {
       "start": {
-        "dateTime": "2023-11-27T08:20:00.0000000",
+        "dateTime": "2023-11-30T08:15:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-27T08:35:00.0000000",
+        "dateTime": "2023-11-30T08:30:00.0000000",
         "timeZone": "UTC"
       },
       "showAs": "busy"
     },
     {
       "start": {
-        "dateTime": "2023-11-27T08:40:00.0000000",
+        "dateTime": "2023-11-30T09:30:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-27T10:10:00.0000000",
+        "dateTime": "2023-11-30T11:00:00.0000000",
         "timeZone": "UTC"
       },
-      "showAs": "busy"
+      "showAs": "oof"
     },
     {
       "start": {
-        "dateTime": "2023-11-27T12:30:00.0000000",
+        "dateTime": "2023-11-30T11:00:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-27T12:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-27T14:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T14:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T08:15:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T10:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T11:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T11:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T12:00:00.0000000",
+        "dateTime": "2023-11-30T11:30:00.0000000",
         "timeZone": "UTC"
       },
       "showAs": "tentative"
     },
     {
       "start": {
-        "dateTime": "2023-11-29T08:15:00.0000000",
+        "dateTime": "2023-11-30T12:00:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-29T08:30:00.0000000",
+        "dateTime": "2023-11-30T14:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T07:45:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:00:00.0000000",
         "timeZone": "UTC"
       },
       "showAs": "busy"
     },
     {
       "start": {
-        "dateTime": "2023-11-29T12:30:00.0000000",
+        "dateTime": "2023-12-01T08:00:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-29T12:45:00.0000000",
+        "dateTime": "2023-12-01T08:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T08:15:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:30:00.0000000",
         "timeZone": "UTC"
       },
       "showAs": "busy"
     },
     {
       "start": {
-        "dateTime": "2023-11-29T14:30:00.0000000",
+        "dateTime": "2023-12-01T12:30:00.0000000",
         "timeZone": "UTC"
       },
       "end": {
-        "dateTime": "2023-11-29T14:45:00.0000000",
+        "dateTime": "2023-12-01T12:45:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T13:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T13:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T16:00:00.0000000",
         "timeZone": "UTC"
       },
       "showAs": "busy"
     }
   ],
   [
-    {
-      "start": {
-        "dateTime": "2023-11-27T08:20:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T08:35:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "tentative"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-27T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T08:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "free"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-27T12:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T12:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "tentative"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T08:15:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "tentative"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T11:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T12:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-29T08:15:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-29T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "tentative"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-29T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-29T08:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "free"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-29T12:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-29T12:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "tentative"
-    },
     {
       "start": {
         "dateTime": "2023-11-30T08:15:00.0000000",
@@ -221,75 +133,97 @@ function updateCalendars() { jsonString = [
         "timeZone": "UTC"
       },
       "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-11-30T14:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-11-30T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T08:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T08:15:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T08:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:45:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "free"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T12:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T12:45:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T13:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T13:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T16:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T16:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T20:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "free"
     }
   ],
   [
-    {
-      "start": {
-        "dateTime": "2023-11-27T08:20:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T08:35:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-27T12:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-27T12:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T08:15:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-28T11:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-28T12:00:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-29T08:15:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-29T08:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
-    {
-      "start": {
-        "dateTime": "2023-11-29T12:30:00.0000000",
-        "timeZone": "UTC"
-      },
-      "end": {
-        "dateTime": "2023-11-29T12:45:00.0000000",
-        "timeZone": "UTC"
-      },
-      "showAs": "busy"
-    },
     {
       "start": {
         "dateTime": "2023-11-30T08:15:00.0000000",
@@ -325,6 +259,17 @@ function updateCalendars() { jsonString = [
     },
     {
       "start": {
+        "dateTime": "2023-11-30T14:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-11-30T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
         "dateTime": "2023-12-01T08:00:00.0000000",
         "timeZone": "UTC"
       },
@@ -333,6 +278,61 @@ function updateCalendars() { jsonString = [
         "timeZone": "UTC"
       },
       "showAs": "tentative"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T08:15:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T08:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T12:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T12:45:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T13:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T13:30:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T16:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "busy"
+    },
+    {
+      "start": {
+        "dateTime": "2023-12-01T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "end": {
+        "dateTime": "2023-12-01T15:00:00.0000000",
+        "timeZone": "UTC"
+      },
+      "showAs": "free"
     }
   ]
-];const jsonData = JSON.parse(JSON.stringify(jsonString, null, 2)); const arrayOfArrays = jsonData.map(item => Object.values(item));setCurrentStatus(arrayOfArrays[0], 'philip-status');setCurrentStatus(arrayOfArrays[1], 'adam-status');setCurrentStatus(arrayOfArrays[2], 'andreas-status');} setInterval(updateCalendars, 5000);
+];const jsonData = JSON.parse(JSON.stringify(jsonString, null, 2)); const arrayOfArrays = jsonData.map(item => Object.values(item));setCurrentStatus(arrayOfArrays[0], 'philip-status');setCurrentStatus(arrayOfArrays[1], 'adam-status');setCurrentStatus(arrayOfArrays[2], 'andreas-status');} setInterval(updateCalendars, 2000);
